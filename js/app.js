e6e298816906976b634ebd8d437d86b304d53c41
Sandra Velez <sandyvelez@gmail.com>
2017-11-12 14:43:21 -0500

    mvvm and knockout stub

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
-var map;
-function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 52.332525, lng: -0.076398},
-        zoom: 12
-    });
-}
 
-initMap();
+// let dataModel = {};
+// let map;
+
+let appView  = {
+
+    init: function() {
+        // Constructor creates a new map - only center and zoom are required.
+        let map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 52.332525, lng: -0.076398},
+            zoom: 12
+        });
+    }
+};
+
+let viewModel = {
+    // firstName: "Bert",
+    // lastName: "Bertington",
+
+    init: function() {
+        appView.init();
+    }
+};
+
+ko.applyBindings(viewModel);
+viewModel.init();
