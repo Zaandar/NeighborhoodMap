834d817d1a9c8ce95d38a5cd79fe1f9aec15fd43
Sandra Velez <sandyvelez@gmail.com>
2017-11-13 21:07:10 -0500

    Added list view and knockout binding

--- js/app.js
+++ js/app.js
@@ -1,18 +1,39 @@
 let map;
 
-class ViewModel {
+class View{
+
+}
 
 
-    // firstName: "Bert",
-    // lastName: "Bertington",
-
+class Model{
     constructor() {
-        this.initMaps();
-        this.createMarkers();
+        let self = this;
+
+        self.poi = ko.observableArray([
+            { title: 'Spectrum Stadium', location: {lat: 28.608254, lng: -81.192621}},
+            { title: 'Computer Science Department', location: {lat: 28.60054, lng: -81.197614}},
+            { title: 'Limbitless Solutions', location: {lat: 28.60687, lng: -81.196695}},
+            { title: 'Central Florida Research Park', location: {lat: 28.587104, lng: -81.199559}},
+            { title: 'Lazy Moon', location: {lat: 28.598338, lng: -81.219712}},
+        ]);
+    }
+}
+
+// TODO understand scope in a class
+class ViewModel {
+    constructor() {
+        let self = this;
+
+        self.model = new Model();
+        self.view = new View();
+
+        self.mapBounds = new google.maps.LatLngBounds();
+
+        self.initMaps();
+        self.createMarkers();
     }
 
     initMaps() {
-
         map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 28.602427, lng: -81.20006},
             zoom: 15
@@ -20,20 +41,12 @@ class ViewModel {
     }
 
     createMarkers() {
-        let poi = [
-            {title: 'Spectrum Stadium', location: {lat: 28.608254, lng: -81.192621}},
-            {title: 'Computer Science Department', location: {lat: 28.60054, lng: -81.197614}},
-            {title: 'Limbitless Solutions', location: {lat: 28.60687, lng: -81.196695}},
-            {title: 'Central Florida Research Park', location: {lat: 28.587104, lng: -81.199559}},
-            {title: 'Lazy Moon', location: {lat: 28.598338, lng: -81.219712}},
-        ];
-
         let poiMarkers = [];
 
-        for (let x = 0; x < poi.length; x++) {
+        for (let x = 0; x < this.model.poi().length; x++) {
 
-            let poiLocation = poi[x].location;
-            let poiTitle = poi[x].title;
+            let poiLocation = this.model.poi()[x].location;
+            let poiTitle = this.model.poi()[x].title;
 
             let poiMarker = new google.maps.Marker({
                 map: map,
@@ -53,13 +66,18 @@ class ViewModel {
                 infoWindow.open(map, poiMarker);
             });
 
-            let mapBounds = new google.maps.LatLngBounds();
-            mapBounds.extend((poiMarkers[x]).position);
+            let point = new google.maps.LatLng(poiLocation.lat, poiLocation.lng);
+            this.mapBounds.extend(point);
         }
     }
 }
 
 // callback from script load in html file
-function startApp() {
-    ko.applyBindings(new ViewModel());
+function runApp() {
+    let viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+}
+
+function handleError(){
+    alert("Error loading maps");
 }
